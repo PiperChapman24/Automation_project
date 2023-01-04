7f1abb848d4e0850e79a2e00e14ab680b7e32b68
PiperChapman24 <PiperChapman24@gmail.com>
2023-01-04 07:08:51 +0000

    final task3 commit

--- automation.sh
+++ automation.sh
@@ -17,28 +17,38 @@ s3bucket="upgradtanvi"
 
 
 cd /var/tmp
-tar -cvf ${myname}-httpd-logs-${timestamp}.tar /var/log/apache2
+tar -cvf ${myname}-httpd-logs-${timestamp}.tar /var/log/apache
 cd
 
-sudo apt update
+#sudo apt update
 sudo apt install awscli
 
 aws s3 \
 cp /var/tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3bucket}/${myname}-httpd-logs-${timestamp}.tar
 
-webpage="/var/www/html"
-if [[ ! -f ${webpage}/inventory.html ]];
+ 
+if [[ $(test -f /var/www/html/inventory.html) > 0 ]];
 then
-	echo -e "LogType\tTimeCreated\tType\tSize">>${webpage}/inventory.html
-fi
-if [[ -f ${webpage}/inventory.html ]];
+	echo "file is not present"
+	cd /var/www/html
+	touch inventory.html
+	echo -e "LogType\tTimeCreated\tType\tSize">>inventory.html
+
+fi	
+if [[ $(test -f /var/www/html/inventory.html) -eq "0" ]];
 then
+        echo "file is  present"
 	filesize=$(du -h /var/tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}')
-	echo -e "httpd-logs\t${timestamp}\ttar\t${filesize}">>${webpage}/inventory.html
+	 echo -e "httpd-logs\t${timestamp}\ttar\t${filesize}">>/var/www/html/inventory.html
 fi
 
-if [[ ! -f /etc/cron.d/automation ]];
+
+
+
+
+
+if [[ $(test -f /etc/cron.d/automation) > 0 ]];
 then
 	echo "0 10 * * * bash root/Automation_Project/automation.sh">> /etc/cron.d/automation
 fi
